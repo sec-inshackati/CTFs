08b5b9a3d1a7fbcb70adf34ff81d4b6501b3bb88
SamuelePerticarari <samuele.perticarari@gmail.com>
2019-07-30 19:43:12 +0200

    Changed executable name of "D3xterVirtualMachine".

--- Challenges/Jeopardy/medium/vm/D3xterVirtualMachine/compile.sh
+++ Challenges/Jeopardy/medium/vm/D3xterVirtualMachine/compile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc src/main.c src/registers.c -D DEBUG -masm=intel -fno-stack-protector -no-pie -Fno-pie -O0 -o dist/SmallVirualMachine_DEBUG
-gcc src/main.c src/registers.c -masm=intel -fno-stack-protector -no-pie -Fno-pie -O0 -o dist/SmallVirualMachine
+gcc src/main.c src/registers.c -D DEBUG -masm=intel -fno-stack-protector -no-pie -Fno-pie -O0 -o dist/D3xterVirtualMachine_DEBUG
+gcc src/main.c src/registers.c -masm=intel -fno-stack-protector -no-pie -Fno-pie -O0 -o dist/D3xterVirtualMachine
